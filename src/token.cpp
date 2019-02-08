b36a3dca0134bc5e554327182d139ccef9234d1c
b <b@unyu>
2019-02-07 16:55:07 -0800

    color, removed files

--- src/token.cpp
+++ src/token.cpp
@@ -1,18 +1,33 @@
 #include "token.hpp"
+#include "color.hpp"
 
 namespace lexer {
 
+static String const col_op   = to_string(Color::yellow);
+static String const col_name = to_string(Color::cyan);
+static String const col_kw   = to_string(Color::yellow);
+static String const col_num  = to_string(Color::red);
+static String const col_off  = to_string(Color::off);
+
 String to_string(Token const& t) {
     return t.match(
-        [](TokenLPar) { return "(LPar)"s; },
-        [](TokenRPar) { return "(RPar)"s; },
-        [](TokenIf)   { return "(If)"s; },
-        [](TokenElse) { return "(Else)"s; },
+        [](TokenLPar) { 
+            return col_op + "(LPar)" + col_off; 
+        },
+        [](TokenRPar) { 
+            return col_op + "(RPar)" + col_off; 
+        },
+        [](TokenIf)   { 
+            return col_kw + "(If)" + col_off; 
+        },
+        [](TokenElse) { 
+            return col_kw + "(Else)" + col_off; 
+        },
         [](TokenName const& name) {
-            return "(Name " + name.value + ")";
+            return col_name + "(Name " + name.value + ")" + col_off;
         },
         [](TokenNum const& num) {
-            return "(Num " + to_string(num.value) + ")";
+            return col_num + "(Num " + to_string(num.value) + ")" + col_off;
         });
 }
 
